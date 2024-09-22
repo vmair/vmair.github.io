dd68ded9d9b00ea893275fdd150229aa6f84c338
vmair <bugme@live.com>
2024-09-22 23:44:15 +0800

    Update just-the-docs.js

--- assets/js/just-the-docs.js
+++ assets/js/just-the-docs.js
@@ -102,7 +102,6 @@ function initSearch() {
 
       lunr.tokenizer.separator = {{ site.search.tokenizer_separator | default: site.search_tokenizer_separator | default: "/[\s\-/]+/" }}
       var index = lunr(function(){
-        
         this.ref('id');
         this.field('title', { boost: 200 });
         this.field('content', { boost: 2 });
