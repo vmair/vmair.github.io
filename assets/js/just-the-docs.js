28f255cc79d7d25a8e38458aabc626459b59eb8f
vmair <bugme@live.com>
2024-09-22 20:42:28 +0800

    Update just-the-docs.js

--- assets/js/just-the-docs.js
+++ assets/js/just-the-docs.js
@@ -94,7 +94,7 @@ function disableHeadStyleSheets() {
 
 function initSearch() {
   var request = new XMLHttpRequest();
-  request.open('GET', '{{ "assets/js/search-data.json" | relative_url }}', true);
+  request.open('GET', '{{ "assets/js/zzzz-search-data.json" | relative_url }}', true);
 
   request.onload = function(){
     if (request.status >= 200 && request.status < 400) {
